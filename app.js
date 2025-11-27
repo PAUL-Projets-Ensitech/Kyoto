c81f43dc8a06aad8f0a3071fc8d307de825fb557
Poy <paulmuller78210@gmail.com>
2025-11-27 17:18:36 +0100

    Ajout de la fonctionnalité de menu déroulant avec gestion des événements et styles associés

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const Menu = document.querySelector(".liste-nav");
 btnMenu.addEventListener("click", () => {
     Menu.classList.toggle("active");
 });
+
+const allLinks = document.querySelectorAll(".item-nav");
+
+allLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        Menu.classList.toggle("active");
+    });
+});
